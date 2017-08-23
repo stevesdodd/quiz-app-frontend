ca44b5d2256ea63a128856983fa105e51e3e86d2
Stephen Dodd <stephen.dodd@rea-group.com>
2017-08-23 11:52:14 +1000

    Add basic router and structure
    - add quiz page component
    - add react router
    - add index route

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import QuizzesPageContainer from './QuizzesPageContainer'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact path='/'
+            component={() => <QuizzesPageContainer/>}>
+          </Route>
+          <Route render={function() {
+            return <p>Not Found</p>
+          }} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/QuizzesPageContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class QuizzesPageContainer extends Component {
+  render() {
+    return (
+      <div>Quizzes page</div>
+    )
+  }
+}
+
+export default QuizzesPageContainer
